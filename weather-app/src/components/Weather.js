cad9b267731b7cfd8b291e54d4dccb74df59f3f6
Akash007670 <akaysharma894@gmail.com>
2021-01-20 20:44:38 +0530

    updated weather

--- weather-app/src/components/Weather.js
+++ weather-app/src/components/Weather.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
-const Weather = () => {
-    return (
-        <div>
-            <p>Weather component</p>
-        </div>
-    )
+export class Weather extends React.Component {
+    render() {
+        return (
+            <div>
+                {this.props.city && this.props.country && <p>Location : {this.props.city} , {this.props.country}</p>}
+                {this.props.temprature && <p>Temprature: {this.props.temprature}</p>}
+                {this.props.humidity && <p>Humidity : {this.props.humidity}</p>}
+                {this.props.description && <p>Condition : {this.props.description}</p>}
+            </div>
+        )
+    }
 }
 
-export default Weather;
+export default Weather

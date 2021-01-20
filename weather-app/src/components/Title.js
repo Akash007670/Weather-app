a256ff0d9acb4bec25ec26614e6b9a17649fecc7
Akash007670 <akaysharma894@gmail.com>
2021-01-21 00:13:16 +0530

    Updated

--- weather-app/src/components/Title.js
+++ weather-app/src/components/Title.js
@@ -4,7 +4,8 @@ class Title extends React.Component {
     render() {
         return (
             <div>
-                <h1>Title component</h1>
+                <h1 className = "title-container__title">Weather Finder</h1>
+                <p className = "title-container__subtitle">Find out temprature,conditions and more....</p>
             </div>
         )
     }
